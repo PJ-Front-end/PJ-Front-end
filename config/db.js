4e52d3c30092f9d5db43e978fa3372fc18143e10
nureman <nureman_s@cmu.ac.th>
2023-04-02 06:06:47 +0700

    fix ip

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 exports.connect = () =>{
-    mongoose.connect('mongodb://54.197.77.253:8089/frontend',{
+    mongoose.connect('mongodb://54.174.232.155:8089/frontend',{
         dbName : "frontend"
     }) 
     .then(()=> console.log('Database is connected'))
